1b51c3b854d831d3fa83ab24686ebbd16411b7c2
JakeHansenExperience <jake.hansen95@gmail.com>
2023-09-13 11:18:05 -0500

    First Commit

---
+++ components/chips.vue
@@ -0,0 +1,73 @@
+<v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      330
+    </v-chip>
+
+    <v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      329
+    </v-chip>
+    <v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      328
+    </v-chip>
+    <v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      327
+    </v-chip>
+    <v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      326
+    </v-chip>
+    <v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      325
+    </v-chip>
+    <v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      327
+    </v-chip>
+    <v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      326
+    </v-chip>
+    <v-chip 
+      
+      label
+      color="red"
+      style="transform: rotate(90deg);"
+    >
+      325
+    </v-chip>
